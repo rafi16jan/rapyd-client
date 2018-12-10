3306b3c308cf4e5e39bc05c7fdea02cf604137e3
Farrell Rafi <farrell@odooabc.com>
2018-12-10 09:08:52 +0700

    Add relativePaths

--- scripts/build.js
+++ scripts/build.js
@@ -9,7 +9,7 @@ const OfflinePlugin = require('offline-plugin');
 
 config.output.publicPath = '';
 
-config.plugins.push(new OfflinePlugin({appShell: 'index.html'}));
+config.plugins.push(new OfflinePlugin({appShell: 'index.html', externals: ['index.html'], relativePaths: true}));
 
 config.resolve.alias['react'] = 'preact-compat';
 config.resolve.alias['react-dom'] = 'preact-compat';
